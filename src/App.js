8d5e7e5bf724eea2121b6274a8be9f9cbc432f80
liz-me <grantedbychoice@gmail.com>
2024-06-10 18:31:37 +0100

    react and tailwind setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p>
+         I/O Extended Maiduguri 2024
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
